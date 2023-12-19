f290b5d7ff8a75f9862ac03f3a21a631f8c08c09
lalapopa <labradordeil@mail.ru>
2023-12-19 12:28:16 +0300

    Refactored trim-applicaiton SHIT

--- apps/trim-app.py
+++ apps/trim-app.py
@@ -9,48 +9,74 @@ from screeninfo import get_monitors
 from F16model.utils.find_trim_position import run
 
 
-def menu():
-    global root
-    global V0
-    global H0
-    global text_box
-    global text_V0
-    global text_H0
-    global run_button
+class TrimApp:
+    def __init__(self, parent):
+        self.root = parent
+        self._window_setup()
+        self.V0 = BoxWidget(self.root, "V0 [m/s] = ", 0).take_number()
+        self.H0 = BoxWidget(root, "H0 [m] = ", 1).take_number()
+        self.run_button = self.play_button()
 
-    root = tk.Tk()
-    window_setup()
+        self.text_box = tk.Text(root, height=10, relief="flat")
+        self.text_box.configure(state="disabled")
+        self.text_box.grid(row=4, columnspan=2, padx=10)
 
-    init_value_V0 = BoxWidget("V0 [m/s] = ", 0)
-    V0 = init_value_V0.take_number()
+    def play_button(self):
+        play_button = ttk.Button(
+            self.root,
+            text="Запустить",
+            command=lambda: threading.Thread(target=self.press_button).start(),
+        )
+        play_button.grid(row=3, columnspan=2, sticky=tk.N, pady=5, padx=5)
+        return play_button
 
-    init_value_H0 = BoxWidget("H0 [m] = ", 1)
-    H0 = init_value_H0.take_number()
+    def press_button(self):
+        V0_float = Converter.float_number(self.V0)
+        if not V0_float:
+            return
+        H0_float = Converter.float_number(self.H0)
+        if not H0_float:
+            return
 
-    run_button = play_button_setup()
-    text_box = tk.Text(root, height=10, relief="flat")
-    text_box.configure(state="disabled")
-    text_box.grid(row=4, columnspan=2, padx=10)
+        self.disable_run_button = True
+        self.handle_button(self.run_button)
 
-    root.mainloop()
+        self._write_to_textbox(self.text_box, f"Running ...")
+        u0, alpha, _ = run(V0_float, H0_float)
+        out_text = (
+            f"{'='*10}\nH0 = {H0_float:.1f} m, V0 = {V0_float:.2f} m/s\n{'='*10}\n"
+        )
+        out_text += f"stab = {math.degrees(u0.stab):.4f} degree;\nthrottle = {u0.throttle:.4f} %;\nalpha = {math.degrees(alpha):.4f} degree;\n"
+        self._write_to_textbox(self.text_box, out_text)
 
+        self.disable_run_button = False
+        self.handle_button(self.run_button)
 
-def window_setup():
-    global window_width
-    global window_height
+    def handle_button(self, button):
+        if self.disable_run_button:
+            button["state"] = tk.DISABLED
+        else:
+            button["state"] = tk.NORMAL
 
-    root.title("Trim F16")
+    def _window_setup(self):
+        self.root.title("Trim F16")
 
-    screen_width, screen_height = get_monitor_size()
+        screen_width, screen_height = get_monitor_size()
 
-    window_width = int(screen_width / 4.2)
-    window_height = int(screen_height / 2.8)
+        window_width = int(screen_width / 4.2)
+        window_height = int(screen_height / 2.8)
 
-    # set the position of the window to the center of the screen
-    root.geometry(f"{290}x{window_height}")
-    root.resizable(1, 1)
-    root.columnconfigure(0, weight=1)
-    root.columnconfigure(1, weight=1)
+        # set the position of the window to the center of the screen
+        self.root.geometry(f"{290}x{window_height}")
+        self.root.resizable(1, 1)
+        self.root.columnconfigure(0, weight=1)
+        self.root.columnconfigure(1, weight=1)
+
+    def _write_to_textbox(self, tb, text):
+        tb.configure(state="normal")
+        tb.delete(1.0, tk.END)
+        tb.insert(1.0, text)
+        tb.configure(state="disabled")
 
 
 def get_monitor_size():
@@ -60,33 +86,24 @@ def get_monitor_size():
     return (1920, 1080)
 
 
-def play_button_setup():
-    play_button = ttk.Button(
-        root,
-        text="Запустить",
-        command=lambda: threading.Thread(target=press_button).start(),
-    )
-    play_button.grid(row=3, columnspan=2, sticky=tk.N, pady=5, padx=5)
-    return play_button
-
-
 class BoxWidget:
-    def __init__(self, text, order_number):
+    def __init__(self, root, text, order_number):
+        self.root = root
         self.order = order_number
-        self.label = ttk.Label(root, text=str(text))
+        self.label = ttk.Label(self.root, text=str(text))
         self.label.grid(row=self.order, column=0, padx=5, pady=10, sticky=tk.E)
 
     def take_number(self, float_number=False):
         value = tk.StringVar()
         if float_number:
             vcmd = (
-                root.register(partial(self._only_num_valid, float_valid=True)),
+                self.root.register(partial(self._only_num_valid, float_valid=True)),
                 "%S",
             )
         else:
-            vcmd = (root.register(self._only_num_valid), "%S")
+            vcmd = (self.root.register(self._only_num_valid), "%S")
         box_entry = ttk.Entry(
-            root,
+            self.root,
             validate="key",
             validatecommand=vcmd,
             textvariable=value,
@@ -105,31 +122,6 @@ class BoxWidget:
         return False
 
 
-def press_button():
-    V0_float = Converter.float_number(V0)
-    if not V0_float:
-        return
-    H0_float = Converter.float_number(H0)
-    if not H0_float:
-        return
-    run_button["state"] = tk.DISABLED
-
-    run_text = f"Running ..."
-    write_to_textbox(text_box, run_text)
-    u0, alpha, _ = run(V0_float, H0_float)
-    out_text = f"{'='*10}\nH0 = {H0_float:.1f} m, V0 = {V0_float:.2f} m/s\n{'='*10}\n"
-    out_text += f"stab = {math.degrees(u0.stab):.4f} degree;\nthrottle = {u0.throttle:.4f} %;\nalpha = {math.degrees(alpha):.4f} degree;\n"
-    write_to_textbox(text_box, out_text)
-    run_button["state"] = tk.NORMAL
-
-
-def write_to_textbox(tb, text):
-    tb.configure(state="normal")
-    tb.delete(1.0, tk.END)
-    tb.insert(1.0, text)
-    tb.configure(state="disabled")
-
-
 class Converter:
     def float_number(box):
         try:
@@ -145,4 +137,6 @@ class Notification:
 
 
 if __name__ == "__main__":
-    menu()
+    root = tk.Tk()
+    TrimApp(root)
+    root.mainloop()
